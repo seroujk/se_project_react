9b33a264142a0a1b83ca2d103b8479570f29a21a
SeroujK <seroujguidanian@gmail.com>
2025-07-02 23:06:08 -0700

    Fixed some context issues

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,14 +1,20 @@
 import "./ToggleSwitch.css"
+import { useContext } from "react";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
 function ToggleSwitch({onToggleSwitch}){
+    const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+    
     return <label className="toggle-switch">
-        <input type="checkbox" className="toggle-switch__checkbox" onClick={() => onToggleSwitch(CurrentTemperatureUnitContext)}/>
+        <input 
+            type="checkbox" 
+            className="toggle-switch__checkbox" 
+            onClick={() => onToggleSwitch(currentTemperatureUnit)}
+        />
         <span className="toggle-switch__circle"></span>
         <span className="toggle-switch__text toggle-switch__text_F">F</span>
         <span className="toggle-switch__text toggle-switch__text_C">C</span>
     </label>
-    
 }
 
-export default ToggleSwitch;
+export default ToggleSwitch;    
