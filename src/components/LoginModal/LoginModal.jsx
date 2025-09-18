80ae1344b7adc3d4c1920ac25cb87b5e910225eb
SeroujK <seroujguidanian@gmail.com>
2025-09-17 20:11:58 -0700

    Package and Typo fixes

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -31,7 +31,7 @@ const LoginModal = ({ isOpen, onLogin, onCloseModal, onButtonClick}) => {
       handleLogin={onLogin}
       buttonText="Log In"
       onSubmit={handleSubmit}
-      secondButtonText={"or Sing Up"}
+      secondButtonText={"or Sign Up"}
       onButtonClick={onButtonClick}
     >
       <label htmlFor="email">Email*</label>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,6 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl = process.env.NODE_ENV === "production" 
+  ? "https://api.wtwrtoday.crabdance.com"
+  : "http://localhost:3001";
 
 // This gets ALL the clothing items
 function getItems() {
